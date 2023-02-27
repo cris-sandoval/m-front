610e438b4c348d7dd19c4b4a1287b2706cc41540
6cris9 <cris.sandoval.cf@gmail.com>
2023-02-26 20:02:18 -0500

    hero-2/ responsive sin conectar con  base

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import {useRef} from "react";
 import "./register.css";
 import Mingalogo from "../../image/minga-logo.png";
 import Profile from "../../image/profile.png";
 import Lock1 from "../../image/lock1.png";
 import Correo from "../../image/@.png";
 import Google from "../../image/Google.png";
+import axios from "axios";
 
 export default function Register() {
+  
+  
+  let name = useRef()
+  let email = useRef()
+  let password = useRef()
+  let password2 = useRef()
+  let check = useRef()
+  
+  function handleSubmit(event){
+    
+    event.preventDefault()
+    // console.log(name.current);
+    // console.log(name.current.id);
+    // console.log(name.current.value);
+    // console.log(email.current.id); 
+    // console.log(email.current.value); 
+    // console.log(password.current.id);
+    // console.log(password.current.value);
+    // console.log(password2.current.id);
+    // console.log(password2.current.value);
+    // console.log(check.current.id);
+    // console.log(check.current.value);
+    let data = {
+      [name.current.name]: name.current.value,
+      [email.current.name]: email.current.value,
+      [password.current.name]: password.current.value,
+      [password2.current.name]: password2.current.value,
+    }
+    console.log(data)
+    // axios.post(
+    //   url, 
+    //   data
+    // )
+  }
+  
   return (
     <div className="register">
       <div className="welcomesection">
@@ -21,35 +57,35 @@ export default function Register() {
           </p>
         </div>
       </div>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit} >
         <fieldset>
-          <legend>Name</legend>
-          <input type="text" name="name" id="name" placeholder="Krowl bell" />
+          <legend htmlFor="name">Name</legend>
+          <input ref={name} type="text" name="name" id="name" required placeholder="Krowl bell" />
           <img src={Profile} alt="profile"/>
         </fieldset>
         <fieldset>
-          <legend>Email</legend>
-          <input  type="email" name="name" id="name"  placeholder="DragonBallZ@Krowl.com" />
+          <legend htmlFor="email">Email</legend>
+          <input  ref={email} type="email" name="email" id="email" required placeholder="DragonBallZ@Krowl.com" />
           <img src={Correo} alt="email"/>
         </fieldset>
         <fieldset>
-          <legend>Password</legend>
-          <input type="password" name="name" id="name" placeholder="********" />
+          <legend htmlFor="password">Password</legend>
+          <input ref={password} type="password" name="password" id="password" required placeholder="********" />
           <img src={Lock1} alt="lock"/>
         </fieldset>
         <fieldset>
-          <legend>Password</legend>
-          <input type="password" name="name" id="name" placeholder="********" />
+          <legend htmlFor="password2">Confirm Password</legend>
+          <input ref={password2} type="password" name="password2" id="password2" required placeholder="********" />
           <img src={Lock1} alt="lock"/>
-
         </fieldset>
-        <fieldset className="notification-check">
-          <input  className="check-box" type="checkbox"  name="email-notification" id="email-notification" />
+        <fieldset  htmlFor="check" className="notification-check">
+          <input  ref={check} className="check-box" type="checkbox"  name="check" id="check" required />
           <label htmlFor="email-notification">
             Send notification to my mail
           </label>
         </fieldset>
-        <a href="#" className="sing-up"> Sing up </a>
+        <input type="submit" className="sing-up" value="Sign in" ></input> 
+        {/* <a href="#"  type="submit" > Sign up </a> */}
         <a href="#" className="sing-in-google"> <img src={Google} alt="google-icon" />  Sing in with Google </a>
         <p> Already have on account? <a href="#" className="link">  Log in </a> </p>
         <p> Go back to  <a href="#" className="link"> home page </a> </p>
